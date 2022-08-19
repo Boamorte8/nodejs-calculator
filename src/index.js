821ddce31ed49fe8f46b262b14e6b8f8dcf809bd
Esteban Salazar <esteban.salazar@gorillalogic.com>
2022-08-18 19:21:33 -0500

    feat(project): add how to handle when user enter data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import { printHello } from '#Lib/hello.js';
+import { createInterface } from 'readline';
 
-printHello();
+// 1 Capture entry
+// 2 Validate entry
+// 3 Do the operation
+// 4 Display result
+
+const consoleInterface = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const handleAnswer = (answer) => {
+  if (answer === 'exit') {
+    process.exit(0);
+  } else {
+    askAction();
+  }
+};
+
+const askAction = () => {
+  consoleInterface.question(
+    'Introduce your action or operation\n',
+    handleAnswer
+  );
+};
+
+askAction();
