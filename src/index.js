1513306f43274ffbe2a4e6c67e81e66da633ffcf
Esteban Salazar <esteban.salazar@gorillalogic.com>
2022-08-18 19:54:41 -0500

    feat(project): capture the user entry

--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
-import { createInterface } from 'readline';
+import { promptQuestion } from '#Lib/promptQuestion.js';
 
-// 1 Capture entry
-// 2 Validate entry
-// 3 Do the operation
-// 4 Display result
-
-const consoleInterface = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const handleAnswer = (answer) => {
-  if (answer === 'exit') {
-    process.exit(0);
-  } else {
-    askAction();
-  }
-};
-
-const askAction = () => {
-  consoleInterface.question(
-    'Introduce your action or operation\n',
-    handleAnswer
+(async () => {
+  // 1 Capture entry
+  const userAnswer = await promptQuestion(
+    'Introduce your action or operation:\n'
   );
-};
 
-askAction();
+  // 2 Validate entry
+  // 3 Do the operation
+  // 4 Display result
+  console.log('userAnswer', userAnswer);
+  if (userAnswer === 'exit') {
+    process.exit(0);
+  }
+})();
